2ea0e5be3cec58b86dbfec6441456582c0134037
irfan254321 <irfanirfan254321@gmail.com>
2025-06-16 15:35:25 +0700

    add navbar

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    'index.html',
+    './assets/navbar/*.{html,js,jsx,ts,tsx}',
+    './assets/script/*.{html,js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
