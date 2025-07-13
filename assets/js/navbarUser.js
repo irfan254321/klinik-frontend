c4a306613bf5e2b78cbcaecc25a9cc8a02a6fd52
irfan254321 <irfanirfan254321@gmail.com>
2025-07-13 07:04:23 +0700

    add navbar user

--- assets/js/navbarUser.js
+++ assets/js/navbarUser.js
@@ -3,10 +3,28 @@ const checkUser = () => {
     let userLogin = sessionStorage.getItem("user")
 
     if(userLogin == null) {
-        console.log("gagal")
-    }else{
-        console.log("berhasil")
+        console.log("tidak ada user")
+        fetch("./assets/navbar/base-navbar.html")
+        .then(response => 
+            response.text()
+        )
+        .then(data => {
+            document.getElementById("navbarUser").innerHTML = data;
+        })
+    }else if(userLogin === "Doctor"){
+        fetch("./assets/navbar/doctor-navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbarUser").innerHTML = data;
+        })
+        console.log("dokter")
+    }else if(userLogin === "Employee"){
+        fetch("./assets/navbar/employee-navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbarUser").innerHTML = data;
+        })
+        console.log("pegawai")
     }
-
 }
 
